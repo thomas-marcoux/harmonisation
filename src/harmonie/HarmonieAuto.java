ad1a9cba5f0e71dc79997ca3c44e980abbfe1367
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-03-05 15:53:25 +0100

    main pret a recevoir des arguments

--- src/harmonie/HarmonieAuto.java
+++ src/harmonie/HarmonieAuto.java
@@ -1,7 +1,5 @@
 package harmonie;
 
-import	java.io.IOException;
-
 public class HarmonieAuto {
 
     /**
@@ -9,32 +7,9 @@ public class HarmonieAuto {
      */
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	/*
-	  HarmonieRoot	root = new HarmonieRoot();
-		
-	  if (args.length > 1)
-	  root.start(args);
-	  else
-	  root.showHelp();
-	*/
-
-	try	{
-	    Chant	c = new Chant("lune.chant");
-	    int[]	s = c.getSoprano();
-		
-	    for (int i = 0; i < s.length; ++i)
-		{
-		    System.out.print("soprano[" + i + "] = " + s[i]);
-		    if (s[i] == Chant.REPEAT)
-			System.out.print(" (Chant.REPEAT)");
-		    else if (s[i] == Chant.PAUSE)
-			System.out.print("(Chant.PAUSE)");
-		    System.out.println();
-		}
-	}
-	catch (IOException | ChantFormatException
-	       | EmptyFileException e) {
-	    System.out.println(e);
-	}
+	if (args.length == 0)
+	    CLI.showHelp();
+	else
+	    CLI.parse(args);
     }
 }
