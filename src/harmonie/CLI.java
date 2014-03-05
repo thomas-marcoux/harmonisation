efbdff7d8f49e3c3ca1cd7a1b6de3671294af3fc
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-03-05 15:52:06 +0100

    Traitement des arguments presque termine

--- src/harmonie/CLI.java
+++ src/harmonie/CLI.java
@@ -1,44 +1,64 @@
 package harmonie;
 
+import	java.io.IOException;
+
 public abstract class CLI {
 
-    private static final String	ARG_NAME = "-name";
-    private static final String	ARG_HELP = "-h";
-    private static final String	ARG_MIDI = "-midi";
+    private static final String	OPTION_NAME = "-name";
+    private static final String	OPTION_HELP = "-h";
     private static final String	ARG_LILY = "-ly";
     private static final String	ARG_NOMBRE = "-nombre";
     private static final String	ARG_BEAUTE = "-beaute";
     private static final String	ARG_DOSSIER = "-w";
 
-    private static final String	NAMES = "";
-    private static final String	HELP = "";
+    private static final char	OPTION_PREFIX = '-';
+
+    private static final String	NAMES = "noms";
+    private static final String	HELP = "help";
 
     public static void	parse(String[] args) {
 	Chant	c = null;
 
 	for (int i = 0; i < args.length; ++i) {
-	    if (args[i].equals(ARG_NAME))
-		showNames();
-	    if (args[i].equals(ARG_HELP))
+	    try {
+		switch (args[i]) {
+		case OPTION_NAME :
+		    showNames(); break;
+		case OPTION_HELP :
+		    showHelp(); break;
+		case Midi.OPTION :
+		    i = Midi.exec(c, args, i); break;
+		/*
+		case Lily.OPTION :
+		    i = Lily.exec(c, args, i); break;
+		case Harmonisations.OPTION :
+		    Harmonisation.exec(args, i++); break;
+		case Beaute.OPTION :
+		    Chant = Beaute.exec(args, i++); break;
+		case Dossier.OPTION :
+		    Dossier.exec(args, i); break;
+		*/
+		default :
+		    throw new OptionsFormatException(args[i]);
+		}
+	    }
+	    catch (IOException | OptionsFormatException
+		   | EmptyFileException | ChantFormatException e) {
+		System.out.println(e);
 		showHelp();
-	    if (args[i].equals(ARG_MIDI));
-	    //Midi.exec(Chant, &args[i += 2]);
-	    if (args[i].equals(ARG_LILY));
-	    //Lily.exec(Chant, &args[i += 2]);
-	    if (args[i].equals(ARG_NOMBRE));
-	    //Harmonisation(args[i++]);
-	    if (args[i].equals(ARG_BEAUTE));
-	    //Chant = Beaute(args[i++]);
-	    if (args[i].equals(ARG_DOSSIER));
-	    //Dossier(args);
+	    }
 	}
     }
 
-    private static void	showNames() {
+    public static boolean	isOption(String arg) {
+	return arg.charAt(0) == OPTION_PREFIX;
+    }
+
+    public static void	showNames() {
 	System.out.print(NAMES);
     }
-    private static void	showHelp() {
+    
+    public static void	showHelp() {
 	System.out.print(HELP);
     }
-   
 }
