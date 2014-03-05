55e6779a937abbe91ad87e994f57efa603202f9f
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-03-05 15:50:30 +0100

    Extension de l'exception OptionsFormatException

--- src/harmonie/OptionsFormatException.java
+++ src/harmonie/OptionsFormatException.java
@@ -1,9 +1,14 @@
 package harmonie;
 
-public class OptionsFormatException extends Exception{
+public class OptionsFormatException extends Exception {
     private static final long	serialVersionUID = 2;
 
-    public	OptionsFormatException(String message) {
-	super(message);
+    public	OptionsFormatException(String option) {
+	super("Mauvais argument : " + option);
+    }
+
+    public	OptionsFormatException(String option, int nb_arg) {
+	super("L'option " + option + " prend "
+	      + nb_arg + " arguments en parametre.");
     }
 }
