7f1206295644ceb726dec2c8273e11b867dc6508
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-03-05 15:53:53 +0100

    Classe Midi pret a l'execution de lignes de commande

--- src/harmonie/Midi.java
+++ src/harmonie/Midi.java
@@ -10,6 +10,9 @@ public abstract class Midi {
      * Cette classe contiens les outils pour ecrire un fichier midi.
      */
 
+    public static final String	OPTION = "-midi";
+    public static final int	NB_ARGUMENTS = 2;
+
     private static final String	MIDI_FILE_SUFFIX = ".mid";
 
     private static final int FILE_TYPE = 1;
@@ -18,8 +21,8 @@ public abstract class Midi {
     private static final int TRACK_NUM = 4;
     private static final int VELOCITY = 64;
 
-    private static final int INSTRU_SOPRANO = 74; //Flute
-    private static final int CANAL_SOPRANO = 1;
+    private static final int INSTRU_SOPRA = 74; //Flute
+    private static final int CANAL_SOPRA = 1;
 
     private static final int INSTRU_ALTO = 11; //Vibraphone
     private static final int CANAL_ALTO = 2;
@@ -32,29 +35,59 @@ public abstract class Midi {
 
     private static final int CONST1 = 36;
     private static final int CONST2 = 12;
+
+    public static int	exec(Chant c, String[] args, int i) 
+	throws IOException, EmptyFileException, ChantFormatException,
+	       OptionsFormatException {
+	if (args.length < i + NB_ARGUMENTS
+	    || CLI.isOption(args[i+1]) || CLI.isOption(args[i+2]))
+	    throw new OptionsFormatException(OPTION, NB_ARGUMENTS);
+	if (c == null)
+	    write(args[i+1], args[i+2]);
+	else
+	    write(c, args[i+2]);
+	c = null;
+	return i + NB_ARGUMENTS;
+    }
     
-    static public void write(Chant c) 
-	throws IOException, InvalidMidiDataException {
+    public static void write(String file_in, String file_out) 
+	throws IOException, EmptyFileException, ChantFormatException {
+	write(new Chant(file_in), file_out);
+    }
+
+    public static void write(String file_in) 
+	throws IOException, EmptyFileException, ChantFormatException {
+	write(new Chant(file_in));
+    }
+    
+    public static void write(Chant c) 
+	throws IOException {
 	write(c, c.getTitre() + MIDI_FILE_SUFFIX);
     }
 
-    static public void write(Chant c, String file_out) 
-	throws IOException, InvalidMidiDataException {
+    public static void write(Chant c, String file_out) 
+	throws IOException {
 	File	out = new File(file_out);
-	Sequence	seq = new Sequence
-	    (Sequence.PPQ, RESOLUTION, TRACK_NUM);
 
-	writeTrack(seq, c.getSoprano(), INSTRU_SOPRANO, CANAL_SOPRANO);
-	/*
-	 * writeTrack(seq, c.getAlto(), INSTRU_ALTO, CANAL_ALTO);
-	 * writeTrack(seq, c.getTenor(), INSTRU_TENOR, CANAL_TENOR);
-	 * writeTrack(seq, c.getBasse(), INSTRU_BASSE, CANAL_BASSE);
-	 */
-	MidiSystem.write(seq, FILE_TYPE, out);
+	try {
+	    Sequence	seq = new Sequence
+		(Sequence.PPQ, RESOLUTION, TRACK_NUM);
+	    
+	    writeTrack(seq, c.getSoprano(), INSTRU_SOPRA, CANAL_SOPRA);
+	    /*
+	    writeTrack(seq, c.getAlto(), INSTRU_ALTO, CANAL_ALTO);
+	    writeTrack(seq, c.getTenor(), INSTRU_TENOR, CANAL_TENOR);
+	    writeTrack(seq, c.getBasse(), INSTRU_BASSE, CANAL_BASSE);
+	    */
+	    MidiSystem.write(seq, FILE_TYPE, out);
+	}
+	catch (InvalidMidiDataException e) {
+	    System.out.println(e);
+	}
     }
 
-    static private void	writeTrack
-	(Sequence seq, int[] t, int instru, int canal) 
+    private static void	writeTrack(Sequence seq,
+				   int[] t, int instru, int canal) 
 	throws InvalidMidiDataException {
 	Track	track = seq.createTrack();
 	int	note;
