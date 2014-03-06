a4be69a6946b95b24d204c2045377fe43a27df1b
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-03-06 15:25:05 +0100

    Gestion d'erreur deplacee dans la methode main

--- src/harmonie/CLI.java
+++ src/harmonie/CLI.java
@@ -9,43 +9,37 @@ public abstract class CLI {
     private static final String	ARG_LILY = "-ly";
     private static final String	ARG_NOMBRE = "-nombre";
     private static final String	ARG_BEAUTE = "-beaute";
-    private static final String	ARG_DOSSIER = "-w";
 
     private static final char	OPTION_PREFIX = '-';
 
     private static final String	NAMES = "noms";
     private static final String	HELP = "help";
 
-    public static void	parse(String[] args) {
+    public static void	parse(String[] args) 
+	throws IOException, OptionsFormatException, EmptyFileException,
+	       ChantFormatException {
 	Chant	c = null;
 
 	for (int i = 0; i < args.length; ++i) {
-	    try {
-		switch (args[i]) {
-		case OPTION_NAME :
-		    showNames(); break;
-		case OPTION_HELP :
-		    showHelp(); break;
-		case Midi.OPTION :
-		    i = Midi.exec(c, args, i); break;
+	    switch (args[i]) {
+	    case OPTION_NAME :
+		showNames(); break;
+	    case OPTION_HELP :
+		showHelp(); break;
+	    case Midi.OPTION :
+		i = Midi.exec(c, args, i); break;
 		/*
-		case Lily.OPTION :
-		    i = Lily.exec(c, args, i); break;
-		case Harmonisations.OPTION :
-		    Harmonisation.exec(args, i++); break;
-		case Beaute.OPTION :
-		    Chant = Beaute.exec(args, i++); break;
-		case Dossier.OPTION :
-		    Dossier.exec(args, i); break;
+		  case Lily.OPTION :
+		  i = Lily.exec(c, args, i); break;
+		  case Harmonisations.OPTION :
+		  Harmonisation.exec(args, i++); break;
+		  case Beaute.OPTION :
+		  Chant = Beaute.exec(args, i++); break;
 		*/
-		default :
-		    throw new OptionsFormatException(args[i]);
-		}
-	    }
-	    catch (IOException | OptionsFormatException
-		   | EmptyFileException | ChantFormatException e) {
-		System.out.println(e);
-		showHelp();
+	    case Dossiers.OPTION :
+		i = Dossiers.exec(args, i); break;
+	    default :
+		throw new OptionsFormatException(args[i]);
 	    }
 	}
     }
