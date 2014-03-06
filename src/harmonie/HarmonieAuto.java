ef2d12c1a86f8bce14a9fb701029cf649c24365a
thomas-marcoux <thomas.marcoux.graphi@gmail.com>
2014-03-06 15:24:24 +0100

    Gestion d'erreur deplacee dans la methode main

--- src/harmonie/HarmonieAuto.java
+++ src/harmonie/HarmonieAuto.java
@@ -1,5 +1,7 @@
 package harmonie;
 
+import	java.io.IOException;
+
 public class HarmonieAuto {
 
     /**
@@ -7,9 +9,18 @@ public class HarmonieAuto {
      */
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	if (args.length == 0)
+	if (args.length == 0) {
 	    CLI.showHelp();
-	else
+	    return;
+	}
+	try {
 	    CLI.parse(args);
+	}
+	catch (IOException | OptionsFormatException
+	       | EmptyFileException | ChantFormatException e) {
+	    System.out.println(e);
+	    CLI.showHelp();
+	}
+
     }
 }
